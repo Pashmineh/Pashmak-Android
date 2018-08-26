6080e1aee1ea8d0776b4e652321fd783bbc36209
m.aghajani <m.aghajani@kian.capital>
2018-08-26 15:18:17 +0430

    SpringAnimation in Splash

--- app/src/main/java/app/pashmak/com/pashmak/ui/splash/SplashActivity.kt
+++ app/src/main/java/app/pashmak/com/pashmak/ui/splash/SplashActivity.kt
@@ -1,18 +1,45 @@
 package app.pashmak.com.pashmak.ui.splash
 
 import androidx.databinding.DataBindingUtil
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
 import app.pashmak.com.pashmak.R
 import app.pashmak.com.pashmak.databinding.ActivitySplashBinding
 import app.pashmak.com.pashmak.ui.base.BaseActivity
 
-class SplashActivity: BaseActivity<SplashViewModel, ActivitySplashBinding>()
-{
+
+class SplashActivity : BaseActivity<SplashViewModel, ActivitySplashBinding>() {
     override val binding: ActivitySplashBinding by lazy { DataBindingUtil.setContentView<ActivitySplashBinding>(this, R.layout.activity_splash) }
     override val viewModel: SplashViewModel by getLazyViewModel()
 
     override fun onViewInitialized(binding: ActivitySplashBinding) {
         super.onViewInitialized(binding)
+        runAnimation()
         viewModel.progressSplash()
+    }
+
+    private fun runAnimation() {
+        val scaleXAnim = SpringAnimation(binding.TxtDesc, DynamicAnimation.SCALE_X).apply {
+            val springForce = SpringForce()
+            springForce.finalPosition = 1.5f
+            springForce.stiffness = SpringForce.STIFFNESS_LOW
+            springForce.dampingRatio = SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY
+            spring = springForce
+            setStartVelocity(500f)
+        }
+
+        val scaleYAnim = SpringAnimation(binding.TxtDesc, DynamicAnimation.SCALE_Y).apply {
+            val springForce = SpringForce()
+            springForce.finalPosition = 1.5f
+            springForce.stiffness = SpringForce.STIFFNESS_LOW
+            springForce.dampingRatio = SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY
+            spring = springForce
+            setStartVelocity(500f)
+        }
+
+        scaleXAnim.start()
+        scaleYAnim.start()
     }
 
 }
